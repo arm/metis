44d82db21f01b638b491c025c48cd27a1813cdc9
Din Asotic <din.asotic@arm.com>
2026-05-06 14:45:41 +0200

    fix commit

--- src/metis/cli/entry.py
+++ src/metis/cli/entry.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: Copyright 2025-2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 # SPDX-License-Identifier: Apache-2.0
 
+#cli/entry.py 
+
 import argparse
 from datetime import datetime
 import logging
